06b5e6cdc3c716ca85734523c9df13b5cbcb5e83
knocniel <81537725+knocniel@users.noreply.github.com>
2024-03-30 16:24:48 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    PIXEL_ARRAY.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    PIXEL_ARRAY.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    PIXEL_ARRAY.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    PIXEL_ARRAY.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-    PIXEL_ARRAY.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
-    PIXEL_ARRAY.show()
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 180)
 })
@@ -13,13 +7,10 @@ input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P2, 90)
 })
 input.onButtonPressed(Button.B, function () {
-    PIXEL_ARRAY.clear()
-    PIXEL_ARRAY.show()
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 0)
 })
-let PIXEL_ARRAY: neopixel.Strip = null
-PIXEL_ARRAY = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+let PIXEL_ARRAY = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 basic.forever(function () {
     basic.pause(100)
     PIXEL_ARRAY.rotate(1)
