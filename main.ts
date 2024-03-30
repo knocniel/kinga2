8c8de97f1a3d8636817a5aea71dcce914398f0fa
knocniel <81537725+knocniel@users.noreply.github.com>
2024-03-30 17:16:07 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    PIXEL_ARRAY.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Green))
-    PIXEL_ARRAY.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Green))
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
 })
@@ -9,10 +7,7 @@ input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P2, 90)
 })
 input.onButtonPressed(Button.B, function () {
-    PIXEL_ARRAY.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Red))
-    PIXEL_ARRAY.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Red))
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 0)
 })
-let PIXEL_ARRAY: neopixel.Strip = null
-PIXEL_ARRAY = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+let PIXEL_ARRAY = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
