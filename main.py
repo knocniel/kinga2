d2841aef4251bb87c7d228d187ba32da7824c17f
knocniel <81537725+knocniel@users.noreply.github.com>
2024-03-30 16:13:06 +0000

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+def on_button_pressed_a():
+    PIXEL_ARRAY.set_pixel_color(0, neopixel.colors(NeoPixelColors.RED))
+    PIXEL_ARRAY.set_pixel_color(1, neopixel.colors(NeoPixelColors.YELLOW))
+    PIXEL_ARRAY.set_pixel_color(2, neopixel.colors(NeoPixelColors.GREEN))
+    PIXEL_ARRAY.set_pixel_color(3, neopixel.colors(NeoPixelColors.BLUE))
+    PIXEL_ARRAY.set_pixel_color(4, neopixel.colors(NeoPixelColors.PURPLE))
+    PIXEL_ARRAY.show()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    PIXEL_ARRAY.clear()
+    PIXEL_ARRAY.show()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+PIXEL_ARRAY: neopixel.Strip = None
+PIXEL_ARRAY = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+
 def on_forever():
-    pass
+    basic.pause(100)
+    PIXEL_ARRAY.rotate(1)
+    PIXEL_ARRAY.show()
 basic.forever(on_forever)
